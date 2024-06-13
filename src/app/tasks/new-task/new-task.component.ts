9246d840284ad899a1c96d3c03832c97b5b69627
VladyslavKozlov <vladislavkozlov2101@gmail.com>
2024-06-13 20:59:31 +0300

    LearnAngular

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Output() cancel = new EventEmitter<void>();
+  @Output() addNew = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSumamry = '';
+  enteredDate = '';
+  onCancel() {
+    this.cancel.emit();
+  }
+  onSubmit() {
+    this.addNew.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSumamry,
+      dueDate: this.enteredDate,
+    });
+  }
+}
---
+++ src/app/tasks/task/task.model.ts
@@ -0,0 +1,15 @@
+export interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
+
+export interface NewTaskData {
+  id?: string;
+  userId?: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
